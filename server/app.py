48436b4323caf84ca923b50082c93fd3a7db91bb
ndurushima <ndu1994@gmail.com>
2025-08-25 12:24:27 -0500

    Articles and article int id routes. I am not sure if it is working by it passes the tests

--- server/app.py
+++ server/app.py
@@ -27,8 +27,14 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    session["page_views"] = session.get("page_views", 0) + 1
 
+    if session["page_views"] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+    
+    article = Article.query.get_or_404(id)
+    data = ArticleSchema().dump(article)
+    return jsonify(data), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
